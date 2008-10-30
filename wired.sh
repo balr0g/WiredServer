9da3c420ccadf69a5ac662aaa3d6831675ff004e
morris <morris@94223b32-352b-4d6d-a509-7b239cf53eea>
2008-10-30 23:59:47 +0000

    Sync with trunk
    git-svn-id: file:///sources/wired/zanka-full-svn/branches/P7/WiredServer@6294 94223b32-352b-4d6d-a509-7b239cf53eea

--- wired.sh
+++ wired.sh
@@ -9,13 +9,11 @@ else
 	CFLAGS="$CFLAGS -O2"
 fi
 
-cd wired
-
-BUILD=$(./config.guess)
+BUILD=$("$SRCROOT/wired/config.guess")
 
 for i in $ARCHS; do
 	if [ ! -f "$PROJECT_TEMP_DIR/make/$i/Makefile" ]; then
-		HOST="$i-apple-darwin$RELEASE"
+		HOST="$i-apple-darwin$(uname -r)"
 		ARCH_CFLAGS="$CFLAGS"
 		ARCH_CPPFLAGS="$CPPFLAGS"
 
@@ -29,22 +27,17 @@ for i in $ARCHS; do
 
 		ARCH_CPPFLAGS="$ARCH_CPPFLAGS -isysroot $SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
 
-		SDKROOT=$(eval echo SDKROOT_$i); SDKROOT=$(eval echo \$$SDKROOT)
-		MACOSX_DEPLOYMENT_TARGET=$(eval echo MACOSX_DEPLOYMENT_TARGET_$i); MACOSX_DEPLOYMENT_TARGET=$(eval echo \$$MACOSX_DEPLOYMENT_TARGET)
-		RELEASE=$(uname -r)
-		BUILD=$("$SRCROOT/wired/config.guess")
-		
+		cd "$SRCROOT/wired"
 		CC="gcc -arch $i" CFLAGS="$ARCH_CFLAGS" CPPFLAGS="$ARCH_CPPFLAGS -I$PROJECT_TEMP_DIR/make/$i" ./configure --build="$BUILD" --host="$HOST" --enable-warnings --srcdir="$SRCROOT/wired" --with-objdir="$OBJECT_FILE_DIR/$i" --with-rundir="$PROJECT_TEMP_DIR/run/$i/wired" --prefix="$PROJECT_TEMP_DIR/Package/Contents/Library" --with-fake-prefix="/Library" --with-wireddir="Wired2.0" --mandir="$PROJECT_TEMP_DIR/Package/Contents/usr/local/man" --without-libwired || exit 1
 		
 		mkdir -p "$PROJECT_TEMP_DIR/make/$i/libwired" "$PROJECT_TEMP_DIR/run/$i" "$BUILT_PRODUCTS_DIR"
 		mv config.h Makefile "$PROJECT_TEMP_DIR/make/$i/"
 		cp -r run "$PROJECT_TEMP_DIR/run/$i/wired"
 
-		cd libwired
+		cd "$SRCROOT/wired/libwired"
 		CC="gcc -arch $i" CFLAGS="$ARCH_CFLAGS" CPPFLAGS="$ARCH_CPPFLAGS -I$PROJECT_TEMP_DIR/make/$i/libwired" ./configure --build="$BUILD" --host="$HOST" --enable-warnings --enable-ssl --enable-pthreads --enable-libxml2 --enable-p7 --srcdir="$SRCROOT/wired/libwired" --with-objdir="$OBJECT_FILE_DIR/$i" --with-rundir="$PROJECT_TEMP_DIR/run/$i/wired/libwired" || exit 1
 		mv config.h Makefile "$PROJECT_TEMP_DIR/make/$i/libwired"
 		cp -r run "$PROJECT_TEMP_DIR/run/$i/wired/libwired"
-		cd ..
 	fi
 	
 	cd "$PROJECT_TEMP_DIR/make/$i"
