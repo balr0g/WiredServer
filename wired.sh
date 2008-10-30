42fa1708a908cd420360a4d9723357cae66b8e21
morris <morris@94223b32-352b-4d6d-a509-7b239cf53eea>
2008-10-30 22:44:09 +0000

    Merge from trunk
    git-svn-id: file:///sources/wired/zanka-full-svn/branches/P7/WiredServer@6288 94223b32-352b-4d6d-a509-7b239cf53eea

--- wired.sh
+++ wired.sh
@@ -1,33 +1,52 @@
 #!/bin/sh
 
-BUILDDIR="$1"
-INSTALLDIR="$2"
+PATH="/opt/local/bin:/usr/local/bin:/usr/bin:/bin"
+CFLAGS="-gdwarf-2"
+
+if echo $CONFIGURATION | grep -q Debug; then
+	CFLAGS="$CFLAGS -O0"
+else
+	CFLAGS="$CFLAGS -O2"
+fi
+
+cd wired
+
+BUILD=$(./config.guess)
 
 for i in $ARCHS; do
-	if [ ! -f "$BUILDDIR/make/$i/Makefile" ]; then
+	if [ ! -f "$PROJECT_TEMP_DIR/make/$i/Makefile" ]; then
+		HOST="$i-apple-darwin$RELEASE"
+		ARCH_CFLAGS="$CFLAGS"
+		ARCH_CPPFLAGS="$CPPFLAGS"
+
+		if [ "$i" = "i386" -o "$i" = "ppc" ]; then
+			SDKROOT="$DEVELOPER_SDK_DIR/MacOSX10.4u.sdk"
+			MACOSX_DEPLOYMENT_TARGET=10.4
+		elif [ "$i" = "x86_64" -o "$i" = "ppc64" ]; then
+			SDKROOT="$DEVELOPER_SDK_DIR/MacOSX10.5.sdk"
+			MACOSX_DEPLOYMENT_TARGET=10.5
+		fi
+
+		ARCH_CPPFLAGS="$ARCH_CPPFLAGS -isysroot $SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
+
 		SDKROOT=$(eval echo SDKROOT_$i); SDKROOT=$(eval echo \$$SDKROOT)
 		MACOSX_DEPLOYMENT_TARGET=$(eval echo MACOSX_DEPLOYMENT_TARGET_$i); MACOSX_DEPLOYMENT_TARGET=$(eval echo \$$MACOSX_DEPLOYMENT_TARGET)
 		RELEASE=$(uname -r)
 		BUILD=$("$SRCROOT/wired/config.guess")
-		HOST="$i-apple-darwin$RELEASE"
 		
-		cd "$SRCROOT/wired"
-		CFLAGS="-g -O2 -arch $i -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET" CPPFLAGS="-I$BUILDDIR/make/$i -isysroot $SDKROOT" ./configure --build="$BUILD" --host="$HOST" --enable-warnings --srcdir="$SRCROOT/wired" --with-objdir="$OBJECT_FILE_DIR/$i" --with-rundir="$BUILDDIR/run/$i/wired" --prefix="$INSTALLDIR/Library" --with-fake-prefix="/Library" --with-wireddir="Wired2.0" --mandir="$INSTALLDIR/usr/local/man" --without-libwired || exit 1
+		CC="gcc -arch $i" CFLAGS="$ARCH_CFLAGS" CPPFLAGS="$ARCH_CPPFLAGS -I$PROJECT_TEMP_DIR/make/$i" ./configure --build="$BUILD" --host="$HOST" --enable-warnings --srcdir="$SRCROOT/wired" --with-objdir="$OBJECT_FILE_DIR/$i" --with-rundir="$PROJECT_TEMP_DIR/run/$i/wired" --prefix="$PROJECT_TEMP_DIR/Package/Contents/Library" --with-fake-prefix="/Library" --with-wireddir="Wired2.0" --mandir="$PROJECT_TEMP_DIR/Package/Contents/usr/local/man" --without-libwired || exit 1
 		
-		cd "$SRCROOT/wired/libwired"
-		CFLAGS="-g -O2 -arch $i -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET" CPPFLAGS="-I$BUILDDIR/make/$i/libwired -isysroot $SDKROOT" ./configure --build="$BUILD" --host="$HOST" --enable-warnings --enable-pthreads --enable-libxml2 --enable-p7 --srcdir="$SRCROOT/wired/libwired" --with-objdir="$OBJECT_FILE_DIR/$i" --with-rundir="$BUILDDIR/run/$i/wired/libwired" || exit 1
+		mkdir -p "$PROJECT_TEMP_DIR/make/$i/libwired" "$PROJECT_TEMP_DIR/run/$i" "$BUILT_PRODUCTS_DIR"
+		mv config.h Makefile "$PROJECT_TEMP_DIR/make/$i/"
+		cp -r run "$PROJECT_TEMP_DIR/run/$i/wired"
 
-		mkdir -p "$BUILDDIR/make/$i/libwired" "$BUILDDIR/run/$i" "$BUILT_PRODUCTS_DIR"
-		mv "$SRCROOT/wired/config.h" "$BUILDDIR/make/$i/config.h"
-		mv "$SRCROOT/wired/libwired/config.h" "$BUILDDIR/make/$i/libwired/config.h"
-		mv "$SRCROOT/wired/Makefile" "$BUILDDIR/make/$i/Makefile"
-		mv "$SRCROOT/wired/libwired/Makefile" "$BUILDDIR/make/$i/libwired/Makefile"
-		cp -r "$SRCROOT/wired/run" "$BUILDDIR/run/$i/wired"
-		cp -r "$SRCROOT/wired/libwired/run" "$BUILDDIR/run/$i/wired/libwired"
+		cd libwired
+		CC="gcc -arch $i" CFLAGS="$ARCH_CFLAGS" CPPFLAGS="$ARCH_CPPFLAGS -I$PROJECT_TEMP_DIR/make/$i/libwired" ./configure --build="$BUILD" --host="$HOST" --enable-warnings --enable-ssl --enable-pthreads --enable-libxml2 --enable-p7 --srcdir="$SRCROOT/wired/libwired" --with-objdir="$OBJECT_FILE_DIR/$i" --with-rundir="$PROJECT_TEMP_DIR/run/$i/wired/libwired" || exit 1
+		mv config.h Makefile "$PROJECT_TEMP_DIR/make/$i/libwired"
+		cp -r run "$PROJECT_TEMP_DIR/run/$i/wired/libwired"
+		cd ..
 	fi
 	
-	cd "$BUILDDIR/make/$i"
-	make -f "$BUILDDIR/make/$i/Makefile" || exit 1
+	cd "$PROJECT_TEMP_DIR/make/$i"
+	make -f "$PROJECT_TEMP_DIR/make/$i/Makefile" || exit 1
 done
-
-exit 0
