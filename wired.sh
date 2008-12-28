fe0f6877b8f72362d3f8d59ac495b9a84c63b56b
morris <morris@94223b32-352b-4d6d-a509-7b239cf53eea>
2008-12-28 12:03:06 +0000

    Sync with trunk
    git-svn-id: file:///sources/wired/zanka-full-svn/branches/P7/WiredServer@6465 94223b32-352b-4d6d-a509-7b239cf53eea

--- wired.sh
+++ wired.sh
@@ -12,10 +12,11 @@ fi
 BUILD=$("$SRCROOT/wired/config.guess")
 
 for i in $ARCHS; do
-	if [ ! -f "$PROJECT_TEMP_DIR/make/$i/Makefile" ]; then
+	if [ ! -f "$PROJECT_TEMP_DIR/make/$i/Makefile" -o ! -f "$TARGET_TEMP_DIR/configured" ]; then
 		HOST="$i-apple-darwin$(uname -r)"
 		ARCH_CFLAGS="$CFLAGS"
 		ARCH_CPPFLAGS="$CPPFLAGS"
+		ARCH_CC="$PLATFORM_DEVELOPER_BIN_DIR/gcc-$GCC_VERSION -arch $i"
 
 		if [ "$i" = "i386" -o "$i" = "ppc" ]; then
 			SDKROOT="$DEVELOPER_SDK_DIR/MacOSX10.4u.sdk"
@@ -28,16 +29,18 @@ for i in $ARCHS; do
 		ARCH_CPPFLAGS="$ARCH_CPPFLAGS -isysroot $SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
 
 		cd "$SRCROOT/wired"
-		CC="gcc -arch $i" CFLAGS="$ARCH_CFLAGS" CPPFLAGS="$ARCH_CPPFLAGS -I$PROJECT_TEMP_DIR/make/$i" ./configure --build="$BUILD" --host="$HOST" --enable-warnings --srcdir="$SRCROOT/wired" --with-objdir="$OBJECT_FILE_DIR/$i" --with-rundir="$PROJECT_TEMP_DIR/run/$i/wired" --prefix="$PROJECT_TEMP_DIR/Package/Contents/Library" --with-fake-prefix="/Library" --with-wireddir="Wired2.0" --mandir="$PROJECT_TEMP_DIR/Package/Contents/usr/local/man" --without-libwired || exit 1
+		CC="$ARCH_CC" CFLAGS="$ARCH_CFLAGS" CPPFLAGS="$ARCH_CPPFLAGS -I$PROJECT_TEMP_DIR/make/$i" ./configure --build="$BUILD" --host="$HOST" --enable-warnings --srcdir="$SRCROOT/wired" --with-objdir="$OBJECT_FILE_DIR/$i" --with-rundir="$PROJECT_TEMP_DIR/run/$i/wired" --prefix="$PROJECT_TEMP_DIR/Package/Contents/Library" --with-fake-prefix="/Library" --with-wireddir="Wired2.0" --mandir="$PROJECT_TEMP_DIR/Package/Contents/usr/local/man" --without-libwired || exit 1
 		
 		mkdir -p "$PROJECT_TEMP_DIR/make/$i/libwired" "$PROJECT_TEMP_DIR/run/$i" "$BUILT_PRODUCTS_DIR"
 		mv config.h Makefile "$PROJECT_TEMP_DIR/make/$i/"
 		cp -r run "$PROJECT_TEMP_DIR/run/$i/wired"
 
 		cd "$SRCROOT/wired/libwired"
-		CC="gcc -arch $i" CFLAGS="$ARCH_CFLAGS" CPPFLAGS="$ARCH_CPPFLAGS -I$PROJECT_TEMP_DIR/make/$i/libwired" ./configure --build="$BUILD" --host="$HOST" --enable-warnings --enable-ssl --enable-pthreads --enable-libxml2 --enable-p7 --srcdir="$SRCROOT/wired/libwired" --with-objdir="$OBJECT_FILE_DIR/$i" --with-rundir="$PROJECT_TEMP_DIR/run/$i/wired/libwired" || exit 1
+		CC="$ARCH_CC" CFLAGS="$ARCH_CFLAGS" CPPFLAGS="$ARCH_CPPFLAGS -I$PROJECT_TEMP_DIR/make/$i/libwired" ./configure --build="$BUILD" --host="$HOST" --enable-warnings --enable-ssl --enable-pthreads --enable-libxml2 --enable-p7 --srcdir="$SRCROOT/wired/libwired" --with-objdir="$OBJECT_FILE_DIR/$i" --with-rundir="$PROJECT_TEMP_DIR/run/$i/wired/libwired" || exit 1
 		mv config.h Makefile "$PROJECT_TEMP_DIR/make/$i/libwired"
 		cp -r run "$PROJECT_TEMP_DIR/run/$i/wired/libwired"
+		
+		touch "$TARGET_TEMP_DIR/configured"
 	fi
 	
 	cd "$PROJECT_TEMP_DIR/make/$i"
