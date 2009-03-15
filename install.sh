e44026e2087d3784c8690de5bae7c487ce6b7566
morris <morris@94223b32-352b-4d6d-a509-7b239cf53eea>
2009-03-15 14:16:30 +0000

    Set "Debug" key
    git-svn-id: file:///sources/wired/zanka-full-svn/branches/P7/WiredServer@7115 94223b32-352b-4d6d-a509-7b239cf53eea

--- install.sh
+++ install.sh
@@ -52,6 +52,8 @@ cat <<EOF >"$LIBRARY/LaunchDaemons/com.zankasoftware.WiredServer.plist"
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
+	<key>Debug</key>
+	<true/>
 	<key>Disabled</key>
 	<true/>
 	<key>Label</key>
